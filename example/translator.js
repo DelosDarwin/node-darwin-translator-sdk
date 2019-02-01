23f8ae9c015f8c00afbc6171f72fb3fc55e9c8c2
Vira Rodionova <bepa.rdnv@gmail.com>
2019-02-01 18:53:26 +0200

    update example

--- example/translator.js
+++ example/translator.js
@@ -1,13 +1,12 @@
 const TRANSLATOR_ID = process.env.TRANSLATOR_ID || 'TRANSLATOR-12837912739';
 const MQTT_ENTPOINT = process.env.TRANSLATOR_ID || 'tcp://localhost:1883';
 
-const MQTT            = require('async-mqtt');
-const CoreChannel     = require('../lib/CoreChannel');
-const NLP             = require('../lib/NLP');
-// const Configurator = require('../lib/Configurator');
-const Logger          = require('../utils/Logger.js');
+const MQTT        = require('async-mqtt');
+const CoreChannel = require('../lib/CoreChannel');
+const NLP         = require('../lib/NLP');
+const Logger      = require('../utils/Logger.js');
 
-const logger = Logger('translator');
+const logger = Logger('translator', 5);
 
 async function main() {
     const mqttClient = MQTT.connect(MQTT_ENTPOINT);
@@ -24,30 +23,20 @@ async function main() {
     await coreChannel.init();
 
     // CORE COMMUNICATION
-    const nva = {noun: 'light1', verb: 'on'};
-    const coreResult = await coreChannel.executeNVA({ nva });
-    logger.info('coreChannel.executeNVA', coreResult);
+    const nva = [ { noun: 'light1', verb: 'on' } ]; // or 'light1.on'
+    const coreResult = await coreChannel.executeNVA({ nva, sourceTranslator: TRANSLATOR_ID });
+    logger.info('coreChannel.executeNVA');
+    logger.debug(coreResult);
 
-    coreChannel.onNVAMessage(({nva}) => {
-        logger.info('NVA FROM CORE', nva);
+    coreChannel.onNVAMessage(({ nva }) => {
+        logger.info('NVA FROM CORE');
+        logger.debug(nva);
     });
-
 
     // NLP COMMUNICATION
     const nlpResult = await nlp.textToNVA({ text: 'Hello, NLP' });
-    logger.info('NLP.textToNVA', nlpResult);
-
-
-    // coreChannel.onNVAMessage    (({nva}) => {
-    //     const ezloPayload = ezloPayloadBuilder.converNVAToEzloPayload({nva});
-    //     return ezloClient.send(ezloPayload);
-    // });
-
-
-    // ezloClient.onMessage(({payload}) => {
-    //     const nva = nvaBuilder.converEzloPayloadToNVA({payload});
-    //     return coreChannel.executeNVA({ nva });
-    // });
+    logger.info('NLP.textToNVA');
+    logger.debug(nlpResult);
 }
 
 function waitForEvent(emitter, eventName) {
@@ -55,6 +44,5 @@ function waitForEvent(emitter, eventName) {
         emitter.on(eventName, resolve);
     }); 
 }
-
 
 main().then(logger.info, logger.error);
